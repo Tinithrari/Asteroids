f45bc67eda94e739bb91be6276ffd76c1b93db39
Xavier HEUGUE <xavier.heugue@live.fr>
2017-10-17 15:51:24 +0200

    Gestion evenement

--- Asteroids/Asteroids/Ship.cpp
+++ Asteroids/Asteroids/Ship.cpp
@@ -26,23 +26,147 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * @see Ship.hpp
  */
 
-world::Ship::Ship()
+world::Ship::Ship(): _keyPressed(false), _event(NONE), _orientation(0,1)
 {
 }
 
 
 world::Ship::~Ship()
 {
+
 }
 
 void world::Ship::ProcessEvent(sf::Event & e)
 {
+    checkKeyPressed(e);
+    checkKeyReleased(e);
 }
 
 void world::Ship::Update(float delta)
 {
+    // Si il y a un evenement
+    if (_event & !(control_event::NONE))
+    {
+        if (_event & control_event::FORWARD)
+        {
+            _thrusterForces.x += _orientation.x * Ship::SPEED;
+            _thrusterForces.y += _orientation.y * Ship::SPEED;
+        }
+
+        if (_event & control_event::BACKWARD)
+        {
+            _thrusterForces.x -= -_orientation.x * Ship::SPEED;
+            _thrusterForces.y -= -_orientation.y * Ship::SPEED;
+        }
+
+        if (_event & control_event::ROT_LEFT)
+        {
+            float angle;
+            _ship.rotate(-Ship::ANGLE_MAX * delta);
+            angle = _ship.getRotation();
+
+            // Calcul du vecteur d'orientation
+            _orientation.x = cos(angle);
+            _orientation.y = sin(angle);
+        }
+
+        if (_event & control_event::ROT_RIGHT)
+        {
+            float angle;
+            _ship.rotate(Ship::ANGLE_MAX * delta);
+            angle = _ship.getRotation();
+
+            // Calcul du vecteur d'orientation
+            _orientation.x = cos(angle);
+            _orientation.y = sin(angle);
+        }
+    }
+
+    // Controle de vitesse maximale
+    if (_thrusterForces.x > Ship::SPEED)
+        _thrusterForces.x = Ship::SPEED;
+
+    if (_thrusterForces.x < -Ship::SPEED)
+        _thrusterForces.x = -Ship::SPEED;
+
+    if (_thrusterForces.y > Ship::SPEED)
+        _thrusterForces.y = Ship::SPEED;
+
+    if (_thrusterForces.y < -Ship::SPEED)
+        _thrusterForces.y = -Ship::SPEED;
+
+    _ship.move(_thrusterForces);
+
+    // Controle de position (Todo)
 }
 
 void world::Ship::draw(RenderTarget & target, RenderStates states) const
 {
+}
+
+void world::Ship::checkKeyPressed(sf::Event &e)
+{
+    if (e.type == sf::Event::KeyPressed)
+    {
+        // Si touche haut appuyee
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            _event = (control_event)(_event | control_event::FORWARD);
+            _event = (control_event)(_event | 1);
+        }
+
+        // Si touche gauche appuyee
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            _event = (control_event)(_event | control_event::ROT_LEFT);
+            _event = (control_event)(_event | 1);
+        }
+
+        // Si touche droite appuyee
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            _event = (control_event)(_event | control_event::ROT_RIGHT);
+            _event = (control_event)(_event | 1);
+        }
+
+        // Si touche bas appuyee
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            _event = (control_event)(_event | control_event::BACKWARD);
+            _event = (control_event)(_event | 1);
+        }
+    }
+}
+
+void world::Ship::checkKeyReleased(sf::Event &e)
+{
+    if (e.type == sf::Event::KeyReleased)
+    {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            _event = (control_event)(_event & !control_event::FORWARD);
+        }
+
+        // Si touche gauche appuyee
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            _event = (control_event)(_event & !control_event::ROT_LEFT);
+        }
+
+        // Si touche droite appuyee
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            _event = (control_event)(_event & !control_event::ROT_RIGHT);
+        }
+
+        // Si touche bas appuyee
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            _event = (control_event)(_event & !control_event::BACKWARD);
+        }
+
+        // Remise a zero des evenements si necessaire
+        if (_event == 1)
+            _event == control_event::NONE;
+    }
 }
